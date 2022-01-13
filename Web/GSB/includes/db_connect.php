724e435f06ea28de75bd75df56d3b8eb5e9ef3a0
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-01-13 08:56:22 +0100

    Remove CI4

---
+++ Web/GSB/includes/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+
+	// $host = ***REMOVED*** ;
+	// $dbusername = ***REMOVED*** ;
+	// $dbpassword = ***REMOVED*** ;
+	// $dbname = ***REMOVED***;
+
+	$host = "localhost" ;
+	$dbusername = "root" ;
+	$dbpassword = "" ;
+	$dbname = "gsb";
+
+	$pdo = new PDO("mysql:host=".$host, $dbusername, $dbpassword);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
