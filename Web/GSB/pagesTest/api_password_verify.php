bcfd8e800ea327275bc18f701f13d5a2ccbbf50b
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-01-21 16:53:26 +0100

    Create feesheet API

--- Web/GSB/pagesTest/api_password_verify.php
+++ Web/GSB/pagesTest/api_password_verify.php
@@ -1,5 +1,7 @@
 <?php
 
+    session_start();
+
     header('Content-Type: application/json');
 
     require("../includes/func_util.php");
@@ -21,33 +23,36 @@
             if (isset($post_json_data[$variable_name]) and strlen($post_json_data[$variable_name]) <= $max_leght and strlen($post_json_data[$variable_name]) >= 3 and !empty($post_json_data[$variable_name])) {
                 $array_return[$variable_name] = htmlspecialchars($post_json_data[$variable_name]);
             }else{
-                $return_data = "{$post_json_data[$variable_name]}\n{$variable_name} doit faire un maximume de {$max_leght} charactères et un minimum de 3 charactères.";
+                $return_data = "{$variable_name} doit faire un maximume de {$max_leght} charactères et un minimum de 3 charactères.";
                 $error = true;
             }
         }
         return $array_return;
     }
 
-    if(isset($post_json_data)){
-        $post_data = array(["username", 50], ["password", 255]);
-        $post_data = post_security($post_data);
+    if(isset($_SESSION["id"])){
+        $return_data = "You are already logged";
+    }else{
+        if(isset($post_json_data)){
+            $post_data = array(["username", 50], ["password", 255]);
+            $post_data = post_security($post_data);
 
-        if(!$error){
-            $sqlr = $database->prepare("SELECT `username`, `password`, `id` FROM users WHERE username = :username");
-            $sqlr->bindParam(':username', $post_data["username"]);
-            $sqlr->execute();
-            $sqlr_rows = $sqlr->fetchAll();
-    
-            if (!empty($sqlr_rows)) {
-                if(password_verify($post_data["password"], $sqlr_rows[0]["password"])){
-                    $return_data = "Good password";
-                    session_start();
-                    $_SESSION["id"] = $sqlr_rows[0]["id"];
+            if(!$error){
+                $sqlr = $database->prepare("SELECT `username`, `password`, `id` FROM users WHERE username = :username");
+                $sqlr->bindParam(':username', $post_data["username"]);
+                $sqlr->execute();
+                $sqlr_rows = $sqlr->fetchAll();
+        
+                if (!empty($sqlr_rows)) {
+                    if(password_verify($post_data["password"], $sqlr_rows[0]["password"])){
+                        $return_data = "Good password";
+                        $_SESSION["id"] = $sqlr_rows[0]["id"];
+                    }else{
+                        $return_data = "Bad password";
+                    }
                 }else{
-                    $return_data = "Bad password";
+                    $return_data = "Bad user";
                 }
-            }else{
-                $return_data = "Bad user";
             }
         }
     }
