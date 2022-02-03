0584590e8345b0d5e7e2f632ace437ca3625c99c
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-02-03 13:09:09 +0100

    API update

--- Web/GSB/pagesTest/api_logged.php
+++ Web/GSB/pagesTest/api_logged.php
@@ -1,13 +1,19 @@
 <?php
 
     header('Content-Type: application/json');
-
     session_start();
+    require_once("../includes/func_util.php");
     
     if(isset($_SESSION["id"])){
-        $return_data = "You are logged on : ".$_SESSION["username"];
+        $return_data = [
+            "id" => 1,
+            "message" => "You are logged on : ".$_SESSION["username"]
+        ];
     }else{
-        $return_data = "You are not logged";
+        $return_data = [
+            "id" => 2,
+            "message" => "You are not logged"
+        ];
     }
 
     echo json_encode($return_data);
