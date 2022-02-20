527c2d2bb7c11f42cac6669cd56926af648567e7
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-02-20 21:49:01 +0100

    Update

--- Web/GSB/includes/func_util.php
+++ Web/GSB/includes/func_util.php
@@ -1,10 +1,19 @@
 <?php
 
     $config = [
-        "host" => "localhost",
-        "dbusername" => "root",
-        "dbpassword" => ""
+        "host" => "nothingefdgsb.mysql.db",
+        "dbusername" => "nothingefdgsb",
+        "dbpassword" => "GSBbest2021"
     ];
+
+    if(in_array($_SERVER['REMOTE_ADDR'], ["127.0.0.1"])){
+        $config = [
+            "host" => "localhost",
+            "dbusername" => "root",
+            "dbpassword" => ""
+        ];
+    }
+
     $return_data = [
         "id" => 0,
         "message" => null
