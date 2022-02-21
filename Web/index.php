8aa204e0430973374e8d70be84162118ecdadddc
FAUCITANO Liam <liam.faucitano@gmail.com>
2022-02-21 22:50:32 +0100

    Update

--- Web/index.php
+++ Web/index.php
@@ -17,15 +17,22 @@
 
 	</head>
 	<body>
-		<header id="header" class="">
+		<header id="header">
 			<nav>
-				<ul>
-					<li><a href="GSB.best"><img src="img/logo_top.png" alt="logo top" class="logo_nav"/></a></li>
-				</ul> 
-				<!-- <ul class="at_right" >
-					<li class="hide-small"><a href="/Recrutement">Recrutement</a></li>
-					<li class="hide-medium hide-large"><a href="#MenuSelect">三</a></li>
-				</ul> -->
+				<div>
+					<ul class="at_right" style="position: absolute;width: 97%;">
+						<li class="hide-small"><a href="#">App</a></li>
+						<li class="hide-small"><a href="#">Presation</a></li>
+						<li class="hide-small"><a href="#">Testes</a></li>
+						<li class="hide-small"><a href="#">Acceuil</a></li>
+						<li class="hide-medium hide-large"><a href="#MenuSelect">三</a></li>
+					</ul>
+				</div>
+				<div>
+					<ul>
+						<li><a href="GSB.best"><img src="img/logo_top.png" alt="logo top" class="logo_nav"/></a></li>
+					</ul>
+				</div>
 			</nav>
 			
 			<!-- <div class="progress-container">
@@ -35,17 +42,30 @@
 
   <script src='https://cldup.com/S6Ptkwu_qA.js'></script>
 		<main>
-		<div id="particles-js"></div>
-			<section id="main">
-				<div>
-					<h1>Galaxy Swiss Bourdin</h1>
-					<hr>
-					<h2>Laboratory</h2>
+
+			<section style="position:relative">
+				<div id="particles-js"></div>
+					<div id="main">
+						<div>
+							<h1>Galaxy Swiss Bourdin</h1>
+							<hr>
+							<h2>Laboratory</h2>
+						</div>
+					</div>
+				</div>
+				<div class="custom-shape-divider-bottom-1645479119">
+					<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+						<path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+						<path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+						<path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+					</svg>
 				</div>
 			</section>
+
+
 			
-			
-			<section id="menu">
+
+			<section id="menu" style="position: relative;">
 				<div class="" name="ne-moving_element">
 					<h2 style="margin-bottom: 30px;">Les liens</h2>
 					<div class="row-resp">
@@ -82,8 +102,15 @@
 				</a>
 			</section>
 
-			<section>
-				<div class="div_anim" name="ne-moving_element">
+			<section id="test" style="position: relative;padding-top: 50px;">
+				<div class="custom-shape-divider-top-1645479630">
+					<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+						<path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+						<path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+						<path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+					</svg>
+				</div>
+				<!-- <div class="div_anim" name="ne-moving_element">
 					<h2>Les informations</h2>
 					<h3>Qui somme-nous ?</h3>
 					<div class="resp_text">
@@ -109,10 +136,10 @@
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> -->
 			</section>
 			
-			<section id="contact">
+			<!-- <section id="contact">
 				<div class="div_anim" name="ne-moving_element">
 					<h2>Contact</h2>
 					<h3>Besoin de plus d'informations ? Contacter nous.</h3>
@@ -122,9 +149,9 @@
 							<p><i class="fa fa-envelope small white"></i> website@nothingelse.fr</p>
 						</button>
 					</a>
-			</section>
+			</section> -->
 			
-			<div id="MenuSelect" class="menu-modal">
+			<!-- <div id="MenuSelect" class="menu-modal">
 			  <div class="menu-modal-content">
 				<a href="#Close">
 					<div class="close">
@@ -141,15 +168,13 @@
 					</div>
 				</div>
 			  </div>
-			</div>
+			</div> -->
 			
 		</main>
 		<footer>
-			<a href="https://nothingelse.fr/?site=liMiamiam.fr">
-				<div class="footer">
-					GSB.best ©2021
-				</div>
-			</a>
+			<div class="footer">
+				<p>GSB.best ©2021 by <a href="https://nothingelese.fr/?site=gsb.best">NothingElse.fr</a></p>
+			</div>
 		</footer>
 		<script src="js/main.js"></script>
 	</body>
