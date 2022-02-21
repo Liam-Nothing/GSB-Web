306415cf30d7e737d6b39475c70e1cb702d83a54
FAUCITANO Liam <liam.faucitano@gmail.com>
2022-02-21 23:17:12 +0100

    Update privacy

--- Web/app/api/includes/main.db.php
+++ Web/app/api/includes/main.db.php
@@ -6,7 +6,7 @@
     $post_json_data = (array) json_decode(stripslashes(file_get_contents("php://input")));
 
     if(!in_array($_SERVER['REMOTE_ADDR'], ["127.0.0.1"])){
-        $database = connectDB(***REMOVED***, $config);
+        $database = connectDB("***REMOVED***", $config);
     }else{
         $database = connectDB("gsb", $config);
     }
