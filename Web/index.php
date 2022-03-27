74f46edea6ac3ada984e37f7df56ee936a8120b3
FAUCITANO Liam <liam.faucitano@gmail.com>
2022-03-27 18:49:16 +0200

    Update index

--- Web/index.php
+++ Web/index.php
@@ -21,9 +21,8 @@
 			<nav>
 				<div>
 					<ul class="at_right" style="position: absolute;width: 97%;">
-						<li class="hide-small hide-medium"><a href="#">App</a></li>
-						<li class="hide-small hide-medium"><a href="#">Presation</a></li>
-						<li class="hide-small hide-medium"><a href="#">Testes</a></li>
+						<li class="hide-small hide-medium"><a href="#App">Applications</a></li>
+						<li class="hide-small hide-medium"><a href="#test">Testes</a></li>
 						<li class="hide-small hide-medium"><a href="#">Acceuil</a></li>
 						<li class="hide-large"><a href="#MenuSelect">三</a></li>
 					</ul>
@@ -66,7 +65,7 @@
 			
 
 			<section id="menu" style="position: relative;">
-				<div class="grid grid-2">
+				<div class="grid grid-2 grid-marg-max">
 					<div>
 						<img class="ne-scale ne-transition" name="ne-moving_element" style="max-width:300px;width:100%;border-radius: 10px;" src="img/hero-medicaments.png" alt="exemple code">
 					</div>
@@ -76,13 +75,32 @@
 							<h2>Nos produits</h2>
 							<div class="text_left_border">
 								<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt mauris vitae libero mattis, sed laoreet urna condimentum. Nam eros nibh, dictum eget velit eu, viverra dapibus ex. Quisque efficitur ligula sed orci iaculis, ac viverra nulla tempus. Donec facilisis vel ligula et fermentum. Cras vulputate lacus vitae enim rutrum posuere. Aliquam rutrum eu leo a vestibulum. Nulla tincidunt dui vel feugiat interdum.
+								Toute l'équipe de votre laboratoire de Lyon vous accueille dans une ambiance calme et d'écoute pour réaliser vos prélèvements et analyses médicales. Le laboratoire est équipé de matériels récents très performants, qui nous permettent de réaliser un grand nombre d'analyses et vous garantissent un service de qualité optimale.
 								</p>
 							</div>
-							<div class="btn-container">
+							<!-- <div class="btn-container">
 								<button>En savoir plus</button>
-							</div>
+							</div> -->
 						</div>
+					</div>
+				</div>
+				<div class="grid grid-2 grid-marg-max">
+					<div class="left-transition ne-transition goright" name="ne-moving_element">
+						<div class="text_left">
+							<h6>Tests</h6>
+							<h2>Dépistage Covid19</h2>
+							<div class="text_left_border">
+								<p>
+								Le test de dépistage pour le coronavirus est basé sur une méthode de biologie moléculaire appelée PCR (Polymerase Chain Reaction ou réaction en chaîne par polymérase). C'est une technique de routine utilisée dans pratiquement tous les laboratoires de biologie qui permet d'amplifier spécifiquement un ADN d'intérêt après plusieurs cycles de réactions biochimiques.
+								</p>
+							</div>
+							<!-- <div class="btn-container">
+								<button>En savoir plus</button>
+							</div> -->
+						</div>
+					</div>
+					<div>
+						<img class="ne-scale ne-transition" name="ne-moving_element" style="max-width:300px;width:100%;border-radius: 10px;" src="https://www.saintcyr78.fr/wp-content/uploads/2021/01/Capture-d%E2%80%99%C3%A9cran-2021-01-05-%C3%A0-12.54.59.png" alt="exemple code">
 					</div>
 				</div>
 			</section>
@@ -95,72 +113,68 @@
 						<path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
 					</svg>
 				</div>
-				<!-- <div class="div_anim" name="ne-moving_element">
-					<h2>Les informations</h2>
-					<h3>Qui somme-nous ?</h3>
-					<div class="resp_text">
-						<div class="text_left">
-							<div class="text_left_border">
-								<p>
-									Liam & Evann nous somme des élèves de BTS SIO.<br>
-									Liam & Evann nous somme des élèves de BTS SIO.<br>
-									Liam & Evann nous somme des élèves de BTS SIO.<br>
-								</p>
-							</div>
+
+				<div class="grid grid-3 grid-marg-max" style="margin-bottom:150px;">
+					<div class="down-transition ne-transition ne-card" name="ne-moving_element">
+						<h2>Tests PCR</h2>
+						<div>
+							<p>Il s'agit d'un prélèvement non invasif à l'intérieur du nez. Un écouvillon (sorte de long coton-tige) est inséré dans le nez du patient afin de récolter des cellules nasales profondes.</p>
+							<button class="ne-bn-card">Prendre rendez-vous</button>
+						</div>
+					</div>
+					<div class="down-transition ne-transition ne-card" name="ne-moving_element">
+						<h2>Tests Antigénique</h2>
+						<div>
+							<p>Ceci est un test plus rapide, qui se fait de la meme maniere que le test PCR. Il est toute fois plus rapide, le résultat est donner environs 15 minutes après le test. Il y a peu de faux positif mais la fiabilité de detection n'est pas très bonne.</p>
+							<button class="ne-bn-card">Prendre rendez-vous</button>
+						</div>
+
+					</div>
+					<div class="down-transition ne-transition ne-card" name="ne-moving_element">
+						<h2>Check-Up</h2>
+						<div>
+							<p>En ces temps dificile vous pouvez demander un check-up a fin de determiner si tous se passe bien dans votre organisme.</p>
+							<button class="ne-bn-card">Prendre rendez-vous</button>
 						</div>
 					</div>
 				</div>
-				<div class="div_anim" name="ne-moving_element">
-					<h3>GSB.best c'est quoi ?</h3>
-					<div class="resp_text">
-						<div class="text_left">
-							<div class="text_left_border">
-								<p>
-									Rien tkt c'est bien quand meme.<br>
-								</p>
-							</div>
-						</div>
-					</div>
-				</div> -->
+				<div class="custom-shape-divider-bottom-1645479119">
+					<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+						<path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+						<path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+						<path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+					</svg>
+				</div>
 			</section>
 			
-			<!-- <section id="contact">
-				<div class="div_anim" name="ne-moving_element">
-					<h2>Contact</h2>
-					<h3>Besoin de plus d'informations ? Contacter nous.</h3>
-				</div>
-					<a href="mailto:website@nothingelse.fr">
-						<button class="contact_btn ne-scale" name="ne-moving_element" style="margin-bottom: 10px;">
-							<p><i class="fa fa-envelope small white"></i> website@nothingelse.fr</p>
-						</button>
-					</a>
-			</section> -->
-			
-			<!-- <div id="MenuSelect" class="menu-modal">
-			  <div class="menu-modal-content">
-				<a href="#Close">
-					<div class="close">
-						<i class="fa fa-times"></i>
+			<section id="App" style="position: relative;">
+				<div class="grid grid-2 grid-marg-max">
+					<div>
+						<img class="ne-scale ne-transition" name="ne-moving_element" style="max-width:300px;width:100%;border-radius: 10px;" src="img/logo-animation-fichefrai.svg" alt="exemple code">
 					</div>
-				</a>
-				<div class="modal-content-content">
-					<h1>Menu</h1>
-					<div class="menu_modal">
-						<a href="#">Accueil</a>
-						<a href="#menu">Les menus</a>
-						<a href="#faq">FAQ</a>
-						<a href="#contact">Contact</a>
+					<div class="right-transition ne-transition" name="ne-moving_element">
+						<div class="text_left">
+							<h6>Application</h6>
+							<h2>Gestion de frais</h2>
+							<div class="text_left_border">
+								<p>
+								Toute l'équipe de votre laboratoire de Lyon vous accueille dans une ambiance calme et d'écoute pour réaliser vos prélèvements et analyses médicales. Le laboratoire est équipé de matériels récents très performants, qui nous permettent de réaliser un grand nombre d'analyses et vous garantissent un service de qualité optimale.
+								</p>
+							</div>
+							<div class="btn-container">
+								<button>En savoir plus</button>
+							</div>
+						</div>
 					</div>
 				</div>
-			  </div>
-			</div> -->
+			</section>
 			
 		</main>
 		<footer>
-			<div class="grid grid-2">
+			<div class="grid grid-2" style="padding-top: 15px;">
 				<div>
 					<div class="text_left">
-						<h5 style="font-size: 19px;">Other informations</h5>
+						<h5 style="font-size: 19px;">Autres informations</h5>
 						<p>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt mauris vitae libero mattis, sed laoreet urna condimentum. Nam eros nibh, dictum eget velit eu, viverra dapibus ex. Quisque efficitur ligula sed orci iaculis, ac viverra nulla tempus. Donec facilisis vel ligula et fermentum. Cras vulputate lacus vitae enim rutrum posuere. Aliquam rutrum eu leo a vestibulum. Nulla tincidunt dui vel feugiat interdum.
 						</p>
@@ -179,18 +193,15 @@
 						</div>
 						<div>
 							<div class="text_left">
-								<h5 style="font-size: 19px;">Liens</h5>
-								<p>Link 1</p>
-								<p>Link 1</p>
-								<p>Link 1</p>
-								<p>Link 1</p>
+								<h5 style="font-size: 19px;">Paternaires</h5>
+								<p><a href="https://nothingelse.fr/">NothingElse.fr</a></p>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div class="separation"></div>
-			<p class="last_text" >GSB.best ©2021 by <a href="https://nothingelse.fr/">Nothingelse.fr</a> & <a href="https://EvannForest.com/">EvannForest.com</a></p>
+			<p class="last_text" >GSB.best ©2021 by <a href="https://nothingelse.fr/">Nothingelse.fr</a></p>
 		</footer>
 		<script src="js/main.js"></script>
 	</body>
