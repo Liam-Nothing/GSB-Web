a261d8c249ecb3480c9c5b228f9fdea34be18061
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-03-31 19:55:15 +0200

    Update functs_repare.php

--- Web/app/api/includes/functs_repare.php
+++ Web/app/api/includes/functs_repare.php
@@ -4,6 +4,7 @@
         $data = array(["api", 30, 1], ["php_session_id", 100, 1]);
         $data = data_security($data);
         session_id($data["php_session_id"]);
+        $return_data["you_set_session_id"] = session_id();
     }else{
         $data = array(["api", 30, 1]);
         $data = data_security($data);
