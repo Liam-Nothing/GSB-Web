94c8d3b093bbb1135c0212f9aeb2c1e72748a6e3
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-04-11 22:42:58 +0200

    Update API user_open_session disable users alert

--- Web/app/api/users/api_session_open.php
+++ Web/app/api/users/api_session_open.php
@@ -9,20 +9,26 @@
             $data = data_security($data);
 
             if(!$error){
-                $sqlr = $database->prepare("SELECT `username`, `password`, `id`, `id_role` FROM users WHERE username = :username");
+                $sqlr = $database->prepare("SELECT `username`, `password`, `id`, `id_role`, `disable` FROM users WHERE username = :username");
                 $sqlr->bindParam(':username', $data["username"]);
                 $sqlr->execute();
                 $sqlr_rows = $sqlr->fetchAll();
         
                 if (!empty($sqlr_rows)) {
                     if(password_verify($data["password"], $sqlr_rows[0]["password"])){
-                        $_SESSION["id"] = $sqlr_rows[0]["id"];
-                        $_SESSION["username"] = $sqlr_rows[0]["username"];
-                        $_SESSION["id_role"] = $sqlr_rows[0]["id_role"];
-                        $return_data["id"] = 1;
-                        $return_data["message"] = "Good password";
-                        $return_data["php_session_id"] = session_id();
-                        $return_data["id_role"] = $_SESSION["id_role"];
+						if($sqlr_rows[0]["disable"] == 1){
+							$return_data["id"] = 2;
+							$return_data["message"] = "Your account is disabled !";
+						}else{
+							$_SESSION["id"] = $sqlr_rows[0]["id"];
+							$_SESSION["username"] = $sqlr_rows[0]["username"];
+							$_SESSION["id_role"] = $sqlr_rows[0]["id_role"];
+							$return_data["id"] = 1;
+							$return_data["message"] = "Good password";
+							$return_data["disable"] = $sqlr_rows[0]["disable"];
+							$return_data["php_session_id"] = session_id();
+                            $return_data["id_role"] = $_SESSION["id_role"];
+						}
                     }else{
                         $return_data["id"] = 2;
                         $return_data["message"] = "Bad password";
