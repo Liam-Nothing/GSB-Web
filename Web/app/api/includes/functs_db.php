f193f3e558514b6151fdb97a86f097ef27eb721c
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-04-14 11:17:56 +0200

    Update

--- Web/app/api/includes/functs_db.php
+++ Web/app/api/includes/functs_db.php
@@ -1,12 +1,20 @@
 <?php
-    $config = json_decode(file_get_contents(dirname(__FILE__)."/config.json"), true);
+    $json_config = json_decode(file_get_contents(dirname(__FILE__).'/config.json'), true);
 
-    if(in_array($_SERVER['REMOTE_ADDR'], ["127.0.0.1"])){
+    if(in_array($_SERVER['REMOTE_ADDR'], [$json_config["localhost"]])){
         $config = [
-            "host" => "localhost",
-            "dbusername" => "root",
-            "dbpassword" => ""
+            "host" => $json_config["host"],
+            "dbusername" => $json_config["dbusername"],
+            "dbpassword" => $json_config["dbpassword"]
         ];
+        if(isset($_GET["debug"]) and $_GET["debug"]==="1"){echo "localhost";}
+    }else{
+        $config = [
+            "host" => $json_config["localhost-host"],
+            "dbusername" => $json_config["localhost-username"],
+            "dbpassword" => $json_config["localhost-password"]
+        ];
+        if(isset($_GET["debug"]) and $_GET["debug"]==="1"){echo "online";}
     }
 
     function connectDB($dbname, $config) {
