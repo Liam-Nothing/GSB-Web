f03ab2a0796bc4bc5d237f6c4dbbb4984ef1da1d
FAUCITANO Liam <liam.faucitano@gmail.com>
2022-04-17 00:10:58 +0200

    Update

--- Web/app/api/users/api_admin_update_user.php
+++ Web/app/api/users/api_admin_update_user.php
@@ -1,1 +1,73 @@
 <?php
+
+if (isset($_SESSION["id"])) {
+	if ($_SESSION["id_role"] == 3) {
+		if (isset($data_from_client)) {
+			$data = array(
+				["id_user", 3, 0],
+				["username", 50],
+				["email", 255, 0],
+				["first_name", 50, 0],
+				["last_name", 50, 0],
+				["birth_date", 255, 0],
+				["adress", 100, 0],
+				["city", 50, 0],
+				["hire_date", 40, 0],
+				["id_role", 2, 0],
+				["zipcode", 10, 0]
+			);
+			$data = data_security($data);
+
+			if (!$error) {
+
+				# if XXX is empty set to NULL in db !
+				$sql_resquest_field_list = ["id_user", "username", "email", "first_name", "last_name", "birth_date", "adress", "city", "hire_date", "id_role", "zipcode"];
+				$sqlr_field = [];
+				foreach ($sql_resquest_field_list as $value) {
+					if ($data[$value] != "null") {
+						array_push($sqlr_field, $value);
+					}
+				}
+				unset($value);
+
+				$sqlr_values_update = [];
+				foreach ($sqlr_field as $value) {
+					if ($value != "id_user") {
+						array_push($sqlr_values_update, $value . " = :" . $value);
+					}
+				}
+				unset($value);
+
+				$sqlr = $database->prepare("
+					Update `users`
+					SET " . join(", ", $sqlr_values_update) . " 
+					WHERE id = :id_user;
+				");
+				foreach ($sqlr_field as $value) {
+					$sqlr->bindParam(':' . $value, $data[$value]);
+				}
+				unset($value);
+
+				if ($sqlr->execute()) {
+					$return_data["id"] = 1;
+					$return_data["message"] = "User updated";
+				} else {
+					$return_data["id"] = 2;
+					$return_data["message"] = "Error request";
+				}
+			} else {
+				$return_data["id"] = 2;
+				$return_data["message"] = "Error!";
+			}
+		}
+	} elseif ($_SESSION["id_role"] == 2) {
+		$return_data["id"] = 1;
+		$return_data["message"] = "You are a admin region";
+	} else {
+		$return_data["id"] = 2;
+		$return_data["message"] = "You don't have permission to do that : " . $_SESSION["id_role"];
+	}
+} else {
+	$return_data["id"] = 2;
+	$return_data["message"] = "You are not logged";
+}
