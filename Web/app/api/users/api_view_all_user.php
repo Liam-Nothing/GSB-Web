47cc22b1f31328113f3929378f588d7caeb15d98
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-05-05 12:56:00 +0200

    [Update] API list users (admin)

--- Web/app/api/users/api_view_all_user.php
+++ Web/app/api/users/api_view_all_user.php
@@ -4,7 +4,7 @@ if (isset($_SESSION["id"])) {
 	if ($_SESSION["id_role"] == 3) {
 
 		$sqlr = $database->query("
-			SELECT disable, zipcode, hire_date, city, adress, birth_date, username, email, last_name, first_name, users.id as user_id, role.label as role_label
+			SELECT disable, zipcode, hire_date, city, adress, birth_date, username, email, last_name, first_name, users.id as user_id, role.label as role_label, role.id as role_id
 			FROM users INNER JOIN role
 			ON users.id_role = role.id;
 		");
@@ -12,9 +12,33 @@ if (isset($_SESSION["id"])) {
 
 		if (!empty($sqlr_rows)) {
 			$return_data = array();
-			$return_data["content"] = $sqlr_rows;
+			// $return_data["content"] = $sqlr_rows;
+
+			$return_data["content"] = array();
+
+			foreach ($sqlr_rows as $row) {
+				$newUser = [];
+				$newUser["disable"] = $row["disable"];
+				$newUser["zipcode"] = $row["zipcode"];
+				$newUser["hire_date"] = $row["hire_date"];
+				$newUser["city"] = $row["city"];
+				$newUser["birth_date"] = $row["birth_date"];
+				$newUser["username"] = $row["username"];
+				$newUser["email"] = $row["email"];
+				$newUser["user_id"] = $row["user_id"];
+				$newUser["role_id"] = $row["role_id"];
+				$newUser["role_label"] = $row["role_label"];
+				$newUser["last_name"] = preg_replace('/[\x00-\x1F\x7F-\xFF]/', '?', $row["last_name"]);
+				$newUser["first_name"] = preg_replace('/[\x00-\x1F\x7F-\xFF]/', '?', $row["first_name"]);
+				$newUser["adress"] = preg_replace('/[\x00-\x1F\x7F-\xFF]/', '?', $row["adress"]);
+				$newUser["city"] = preg_replace('/[\x00-\x1F\x7F-\xFF]/', '?', $row["city"]);
+				array_push($return_data["content"], $newUser);
+			}
+
 			$return_data["id"] = 1;
 			$return_data["message"] = "Feesheets";
+
+			// var_dump($return_data);
 		} else {
 			$return_data["id"] = 1;
 			$return_data["message"] = "No feesheet found";
